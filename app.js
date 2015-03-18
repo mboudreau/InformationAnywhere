f9fe6bdade11b7012e91c5bf92ea453c0f1af3ee
Michel Boudreau <michelboudreau@gmail.com>
2015-03-18 17:02:37 +1100

    adding API to routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,7 @@
 
 	angular.module('informationAnywhere', [])
 		.config(function(){
-			console.log('Works Bitches');
+
 		});
 
 })(window, document, angular);
