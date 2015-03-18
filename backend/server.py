424ec74425d6081141449f87b574736ec7bd9a0e
Ryan Djurovich <git@ryandjurovich.com>
2015-03-18 21:56:12 +1100

    Added placeholder index and serving of static content

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,11 @@
-from bottle import route, run
+import os
+from bottle import route, run, static_file
 
 @route('/')
+def send_static():
+    return static_file('index.html', root=os.path.join(os.getcwd()))
+
+@route('/api')
 def hello():
     return "{}"
 
