4ef81c83dd0e48f3c61c0d7db1389de9906be42a
Ryan Djurovich <git@ryandjurovich.com>
2015-03-19 03:12:52 +1100

    Stripped show inventory

--- backend/test.py
+++ backend/test.py
@@ -6,5 +6,24 @@ password = "cisco"
 
 t = Telnet(hostname, username, password)
 t.connect()
-print t.execute([ "show inventory", "show inventory" ])
+inventory = t.execute([ "show inventory" ])
 t.disconnect()
+
+print "raw inventory"
+print inventory
+print "----------"
+print ""
+
+# Split
+
+mylist = inventory.strip().replace("\r","").replace("\n",", ").split(", ")
+[x.strip() for x in mylist]
+
+stuff = {}
+for x in mylist:
+    new = x.split(": ")
+    key = new[0]
+    value = new[1].strip('"').strip()
+    stuff[key] = value
+
+print stuff
