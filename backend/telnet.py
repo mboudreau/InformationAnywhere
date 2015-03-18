0c0c83d40c49c5a5dce1e45d1cd56833e2f9f1b7
Ryan Djurovich <git@ryandjurovich.com>
2015-03-19 02:01:29 +1100

    Using telnetlib instead of sockets

--- backend/telnet.py
+++ backend/telnet.py
@@ -1,41 +1,21 @@
-import socket, select, string, sys
-if __name__ == "__main__":
+import sys
+import telnetlib
 
-    host = "10.10.31.239"
-    port = 23
+HOST = "10.10.31.239"
+user = "red"
+password = "cisco"
 
-    try:
-        skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error, e:
-        print("Error creating socket: %s" % e)
-        sys.exit(1)
+tn = telnetlib.Telnet(HOST)
 
-    try:
-        skt.connect((host, port))
-    except socket.gaierror, e:
-        print("Address-related error connecting to server: %s" % e)
-        sys.exit(1)
-    except socket.error, e:
-        print("Error connecting to socket: %s" % e)
-        sys.exit(1)
-
-    commands = ["red", "cisco", "show inventory"]
-
-    sent = 1;
-
-    for command in commands:
-        skt.send(command+"\n")
-        sent += len(command)
-
-    count = 0
-
-    while 1:
-        count += 1
-        try:
-            buf = skt.recv(4096)
-        except socket.error, e:
-            print("Error receiving data: %s" % e)
-            sys.exit(1)
-        if count == sent:
-            print buf
-            break
+print "until username"
+tn.read_until("Username: ")
+tn.write(user + "\n")
+print "until password"
+tn.read_until("Password: ")
+tn.write(password + "\n")
+print "then"
+tn.write("show inventory\n")
+print "and"
+tn.write("exit\n")
+print "done"
+print tn.read_all()
