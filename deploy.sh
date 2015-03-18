7ecd4d2255f25dfcfa0f34e0fffc1f5a76df9506
Ryan Djurovich <git@ryandjurovich.com>
2015-03-18 21:48:11 +1100

    Added build helpers and update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# Copy files to IOX server (see SSH config in README)
-scp -r * iox:~/information-anywhere/
+# Prepare deploy
+mkdir temp
+cp -R backend/ temp/
+cp frontend/release/information-anywhere/index.html temp/
 
+# Copy files to IOX server (see SSH config in README)
+scp -r temp/* iox:/flash/information-anywhere/
+
+# Cleanup
+rm temp/*
+rmdir temp
