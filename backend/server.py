91965ac951f30ea5a2a75c9363d92312bfa1f056
Ryan Djurovich <git@ryandjurovich.com>
2015-03-19 08:08:19 +1100

    Merge API endpoints for MichEl

--- backend/server.py
+++ backend/server.py
@@ -5,23 +5,24 @@ from bottle import route, run, static_file, request
 def index():
     return static_file('index.html', root=os.path.join(os.getcwd()))
 
-@route('/api/whoami')
-def whoami():
+@route('/api/<mac>')
+def api(mac):
     # Get the client IP address
     ip = request.remote_addr
     # Retrieve info from SMX server
     username = "learning"
     password = "learning"
-    req = urllib2.Request("http://10.10.20.11/api/contextaware/v1/location/clients/10.10.30.166.json")
-    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-    req.add_header("Authorization", "Basic %s" % base64string)   
-    result = urllib2.urlopen(req)
-    data = json.load(result)
-    # Return JSON
-    return { "ip": request.remote_addr, "json": data }
-
-@route('/api/devinfo/<mac>')
-def devinfo(mac):
+    smx_requqest = urllib2.Request(
+        "http://10.10.20.11/api/contextaware/v1/location/clients/10.10.30.166.json"
+        )
+    smx_requqest.add_header(
+        "Authorization",
+        "Basic %s" % base64.encodestring(
+            '%s:%s' % (username, password)
+            ).replace('\n', '')
+        )
+    result = urllib2.urlopen(smx_requqest)
+    smxdata = json.load(result)
     # Note: in production we would use ARP data to retrieve IP, then query
     #       the device using CDP or Device APIs
     from telnet import Telnet
@@ -48,6 +49,6 @@ def devinfo(mac):
         value = temp[1].strip('"').strip()
         data[key] = value
     # Return json
-    return {"status":"Good", "updated":1426679517, "inventory":data}
+    return { "device": { "inventory": data }, "smx": smxdata }
 
 run(host='', port=8080, debug=True)
