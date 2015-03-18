cd513aef7aa101d3b993e4f5f030b845f74c0883
Michel Boudreau <michelboudreau@gmail.com>
2015-03-18 16:35:50 +1100

    adding inline javascript/css, adding bootstrap and angular for front-end

---
+++ public/javascripts/app.js
@@ -0,0 +1,8 @@
+(function (window, document, angular) {
+
+	angular.module('informationAnywhere', [])
+		.config(function(){
+			console.log('Works Bitches');
+		});
+
+})(window, document, angular);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', {
+		helpers: {
+			scripts: function () {
+				var string = '';
+				[
+					'node_modules/angular/angular.min.js',
+					'public/javascripts/app.js',/*,
+					'node_modules/bootstrap/dist/js/bootstrap.min.js'*/
+				].forEach(function(value){
+					string += fs.readFileSync(value, {encoding: 'utf8'});
+					});
+
+				return string;
+			},
+			styles: function () {
+				var string = '';
+				[
+					'node_modules/bootstrap/dist/css/bootstrap.min.css',
+					'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+				].forEach(function(value){
+						string += fs.readFileSync(value, {encoding: 'utf8'});
+					});
+
+				return string;
+			}
+		}
+	});
 });
 
 module.exports = router;
