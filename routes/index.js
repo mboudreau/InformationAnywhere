95b6ba60da39f250e23489c46275ddaa4ee86ddc
Michel Boudreau <michelboudreau@gmail.com>
2015-03-18 19:50:48 +1100

    fixing embedding scripts and styles inline for easy delivery

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,52 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var path = require('path');
+var less = require('less');
+var wait = require('wait.for');
+
+var scripts = '';
+
+[
+	'node_modules/angular/angular.min.js',
+	'public/javascripts/app.js', /*,
+ 'node_modules/bootstrap/dist/js/bootstrap.min.js'*/
+].forEach(function (value) {
+		scripts += fs.readFileSync(value);
+	});
+
+
+var styles = '';
+
+[
+	'public/styles/global.less'
+].forEach(function (value) {
+		//var parser = new less.Parser;
+		var file = fs.readFileSync(value).toString();
+		var lessString = '';
+		//wait.for(fs.readFile,'largeFile.html')
+		less.render(file,
+			{
+				paths: ['.', './public/styles'],  // Specify search paths for @import directives
+				filename: path.resolve(value)// Specify a filename, for better error messages
+			},
+			function (err, output) {
+				if (err) {
+					return console.error(err);
+				}
+				styles += output.css;
+			});
+	});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('index', {
 		helpers: {
 			scripts: function () {
-				var string = '';
-				[
-					'node_modules/angular/angular.min.js',
-					'public/javascripts/app.js',/*,
-					'node_modules/bootstrap/dist/js/bootstrap.min.js'*/
-				].forEach(function(value){
-					string += fs.readFileSync(value, {encoding: 'utf8'});
-					});
-
-				return string;
+				return scripts;
 			},
 			styles: function () {
-				var string = '';
-				[
-					'node_modules/bootstrap/dist/css/bootstrap.min.css',
-					'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
-				].forEach(function(value){
-						string += fs.readFileSync(value, {encoding: 'utf8'});
-					});
-
-				return string;
+				return styles;
 			}
 		}
 	});
