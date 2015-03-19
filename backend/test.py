bb7571e070542491170087002bbde06d7afba85b
Ryan Djurovich <git@ryandjurovich.com>
2015-03-19 11:31:53 +1100

    Restored double newline

--- backend/test.py
+++ backend/test.py
@@ -6,5 +6,5 @@ credentials = Credentials.telnet("E0:89:9D:DA:1E:00")
 # Execute telnet commands to retrieve data
 t = Telnet(credentials["hostname"], credentials["username"], credentials["password"])
 t.connect()
-print t.execute([ "show inventory" ])
+print t.execute([ "show inventory", "show version", "show mac address-table | exclude (Fa0/1|CPU)" ])
 t.disconnect()
